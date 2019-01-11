dd03778d67d077f9a50c062a7b47ed0985be470d
Marcos Jr <marcosjr.lopes@outlook.com>
2019-01-11 21:24:47 -0200

    Primeiro commit.

---
+++ src/main/java/br/com/lpz/mein/batch/config/SpringConfig.java
@@ -0,0 +1,79 @@
+package br.com.lpz.mein.batch.config;
+
+import java.net.MalformedURLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableBatchProcessing
+public class SpringConfig {
+	
+	@Value("org/springframework/batch/core/schema-drop-sqlite.sql")
+	private Resource dropReopsitoryTables;
+	
+	@Value("org/springframework/batch/core/schema-sqlite.sql")
+	private Resource dataReopsitorySchema;
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.sqlite.JDBC");
+		dataSource.setUrl("jdbc:sqlite:repository.sqlite");
+		return dataSource;
+	}
+	
+	public DataSourceInitializer dataSourceInitializer(DataSource dataSource) 
+			throws MalformedURLException{
+		ResourceDatabasePopulator databasePopulator 
+			= new ResourceDatabasePopulator();
+		
+		databasePopulator.addScript(dropReopsitoryTables); //REMOVER OS DADOS. 
+		databasePopulator.addScript(dataReopsitorySchema);//RECRIAR DB
+		databasePopulator.setIgnoreFailedDrops(true);
+		
+		DataSourceInitializer initializer = new DataSourceInitializer();
+		initializer.setDataSource(dataSource);
+		initializer.setDatabasePopulator(databasePopulator);
+		
+		return initializer;
+	}
+	
+	public JobRepository getJobRepository() throws Exception {
+		JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
+		factory.setDataSource(dataSource());
+		factory.setTransactionManager(getTransactionManager());
+		factory.afterPropertiesSet();
+		return (JobRepository) factory.getObject();
+	}
+	
+	private PlatformTransactionManager getTransactionManager() {
+		return new ResourcelessTransactionManager();
+	}
+	
+	public JobLauncher getJobLauncher() throws Exception {
+		SimpleJobLauncher  jobLauncher = new SimpleJobLauncher();
+		jobLauncher.setJobRepository(getJobRepository());
+		jobLauncher.afterPropertiesSet();
+		return jobLauncher;
+		
+	}
+	
+	
+}
